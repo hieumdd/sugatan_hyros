8928bc3870099d69629b3778d6e0244d8a293440
HM <hieumdd@gmail.com>
2021-11-15 23:38:45 +0700

    refactor!: Refactored into controllers, added tasks

---
+++ controller/pipelines.py
@@ -0,0 +1,11 @@
+from controller.hyros import get
+from controller.bigquery import load
+from controller.tasks import AuthHyrosClient
+
+
+def run(client: AuthHyrosClient) -> dict:
+    rows = get(client)
+    return {
+        "num_processed": len(rows),
+        "output_rows": load(f"{client['name']}_Hyros", rows),
+    }
--- test/test_units.py
+++ test/test_units.py
@@ -1,10 +1,18 @@
 from unittest.mock import Mock
 
+import pytest
+
 from main import main
+from controller.tasks import CLIENTS
 
 
-def test_auto():
-    data = {}
+@pytest.mark.parametrize(
+    "client",
+    CLIENTS,
+    ids=[i["name"] for i in CLIENTS],
+)
+def test_pipelines(client):
+    data = client
     req = Mock(get_json=Mock(return_value=data), args=data)
     res = main(req)
     assert res["num_processed"] >= 0
